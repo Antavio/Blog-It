1354c2a209ef831f1c3dcf08bf07dd065031be84
Antavio <njuguna13@gmail.com>
2019-04-27 11:13:07 +0300

    Create a Role Model class and create a relationship with User Class Model.

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://antavio:blogs@localhost/blogs'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevConfig(Config):
     DEBUG = True
